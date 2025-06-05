1de9d8d2ef389718c1123a79717988d99122e0ba
krish <krishnagelli4@gmail.com>
2025-06-05 08:16:25 -0400

    dry code test

--- common.sh
+++ common.sh
@@ -3,3 +3,16 @@ systemd_setup(){
   systemctl enable $component
   systemctl restart $component
 }
+artifacts_setup(){
+  useradd roboshop
+
+  rm -rf /app
+  mkdir /app
+
+  curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
+  cd /app
+  unzip /tmp/cart.zip
+
+  cd /app
+  npm install
+}
