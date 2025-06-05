ff561a23434f30a73cb191344c02231ab9ac22ac
krish <krishnagelli4@gmail.com>
2025-06-05 08:21:41 -0400

    dry code test

--- common.sh
+++ common.sh
@@ -9,9 +9,9 @@ artifacts_setup(){
   rm -rf /app
   mkdir /app
 
-  curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
+  curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
   cd /app
-  unzip /tmp/cart.zip
+  unzip /tmp/$component.zip
 
   cd /app
   npm install
